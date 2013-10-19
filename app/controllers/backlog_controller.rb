6c7d2a3fd2ddaf0f2f943768f0efaabc51363296
B Kleinen <kleinen@infrastructure.de>
2013-10-19 17:45:31 +0200

    issues parse their json

--- app/controllers/backlog_controller.rb
+++ app/controllers/backlog_controller.rb
@@ -1,8 +1,14 @@
 class BacklogController < ApplicationController
   def show
     # https://api.github.com/repos/learnery/learnery/issues
-    i = Issue.new
-    i.title = "Hey."
+    require 'open-uri'
+    owner = URI.escape(params[:owner])
+    repository = URI.escape(params[:repository])
+    f = open("https://api.github.com/repos/#{owner}/#{repository}/issues")
+    json = f.read
+    parsed_json = ActiveSupport::JSON.decode(json)
+    first_issue = parsed_json.first
+    raise first_issue.inspect
     @issues = [ i ]
   end
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,15 @@
 class Issue
   attr_accessor :title, :number, :id, :html_url, :url, :state
+
+  def initialize(json_hash = {})
+    initialize_from_json_hash(json_hash)
+  end
+
+  def initialize_from_json_hash(json_hash)
+    json_hash.each { | k,v|
+      method = "#{k}="
+      send(method,v) if respond_to?(method)
+    }
+  end
+
 end
