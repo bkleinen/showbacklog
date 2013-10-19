cb44f0445ba48041d0b71211d770158ecb4a2dc8
B Kleinen <kleinen@infrastructure.de>
2013-10-19 17:47:40 +0200

    listing issues done

--- app/controllers/backlog_controller.rb
+++ app/controllers/backlog_controller.rb
@@ -7,8 +7,10 @@ class BacklogController < ApplicationController
     f = open("https://api.github.com/repos/#{owner}/#{repository}/issues")
     json = f.read
     parsed_json = ActiveSupport::JSON.decode(json)
-    first_issue = parsed_json.first
-    raise first_issue.inspect
-    @issues = [ i ]
+
+#    first_issue = parsed_json.first
+#    raise first_issue.inspect
+
+    @issues = parsed_json.map { |j| Issue.new(j)}
   end
 end
